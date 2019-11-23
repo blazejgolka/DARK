f00b9a2343cbd22bfbc8b3473195d71ceca99d5e
BlazejGolka <blazej.golka@gmail.com>
2019-11-23 23:16:28 +0100

    Dalsze prace nad tłem

---
+++ Assets/Scripts/cs_LoadScene.cs
@@ -0,0 +1,11 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class cs_LoadScene : MonoBehaviour
+{
+    public void SceneLoader (int SceneIndex) {
+        SceneManager.LoadScene(SceneIndex);
+    }
+}
